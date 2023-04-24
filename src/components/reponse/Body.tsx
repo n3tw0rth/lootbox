06c4a88b7a4d8fa4783917a559b12764dcde1f55
theekshana <rtheekshanafernando@gmail.com>
2023-04-24 23:10:51 +0530

    changes

--- src/components/reponse/Body.tsx
+++ src/components/reponse/Body.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect } from "react";
 
+let objectMap: any = {};
+
 function Body({ res }: { res: any }) {
   return (
     <>
-      <div className="w-full h-full">
-        <TraverseObject obj={res} />
+      <div className="w-full h-full px-6">
+        <div className="w-full h-10 flex items-center justify-start">
+          <button className="px-3 py-1 text-sm bg-gray-100 hover:bg-gray-200">Prettify</button>
+          <button className="px-3 py-1 text-sm bg-gray-100 hover:bg-gray-200">Raw</button>
+        </div>
+
+        <div className="h-full w-full overflow-scroll">
+          <pre>{JSON.stringify(res, null, 2)}</pre>
+        </div>
       </div>
     </>
   );
-}
-
-function TraverseObject({ obj }: { obj: any }) {
-  let level: number = 0;
-  let objectMap: any = {};
 }
 
 export default Body;
