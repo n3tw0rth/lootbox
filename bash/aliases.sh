0e9d5f0d2d7c30152944a2b6d9e1b274f2426872
n3tw0rth <rtheekshanafernando@gmail.com>
2025-12-20 00:00:00 +0530

    chore: added aliases

---
+++ bash/aliases.sh
@@ -0,0 +1,5 @@
+# checkout completed here: https://github.com/n3tw0rth/completed
+alias terraform='completed -t approve,Enter terraform'
+
+# use this to serve markdown files dirs with the chrome plugin https://github.com/md-reader/md-reader
+alias serve-dir="$BROWSER file://$(pwd)"
