906eab135b2c5a5b02c42407ac61715ac4fda362
0xbyt3z <40062006+n3tw0rth@users.noreply.github.com>
2026-07-24 19:58:16 +0530

    chore: housekeeping

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,5 +1,5 @@
-# checkout completed here: https://github.com/n3tw0rth/completed
-alias terraform='completed -t approve,Enter terraform'
+# checkout completed here: https://github.com/n3tw0rth/hark
+alias terraform='hark -t approve,Enter terraform'
 
 # use this to serve markdown files dirs with the chrome plugin https://github.com/md-reader/md-reader
 alias serve-dir="$BROWSER file://$(pwd)"
